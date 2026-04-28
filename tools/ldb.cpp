2c22f8ce048261863a48df3f882e1edbda64775e
joshpetersdev <joshuagpeters99@gmail.com>
2026-04-28 08:18:09 -0600

    Handling user input

--- tools/ldb.cpp
+++ tools/ldb.cpp
@@ -5,24 +5,26 @@
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <editline/readline.h>
+#include <string>
+#include <algorithm>
+#include <sstream>
 
-namespace {
-  pid_t attach(int argc, cont char** argv) {
+namespace { 
+  pid_t attach(int argc, const char** argv) {
     pid_t pid = 0;
 
-    if (argc == 3 && argv[2] == std::string_view("-p") < 0) {
+    if (argc == 3 && argv[1] == std::string_view("-p")) {
       pid = std::atoi(argv[2]);
 
-      if (pic <= 0) {
-        std::cerr << "Invalid Path\n";
+      if (pid <= 0) {
+        std::cerr << "Invalid path\n";
         return -1;
       }
-
       if (ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) < 0) {
-        std::perror("attach failed");
+        std::perror("Attach failed");
         return -1;
       }
-
     }
     else {
       const char* program_path = argv[1];
@@ -32,24 +34,70 @@ namespace {
         return -1;
       }
 
-      if (execlp(program_path, program_path, nullptr) < 0) {
-        std::perror("exec failed");
-        return -1;
-      }
+      if (pid == 0) {
+        if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
+          std::perror("trace failed");
+          return -1;
+        }
 
-      if (ptrace(PTRACE_TRACEME, pid, nullptr, nullptr) < 0) {
-        std::perror("Trace failed");
-        return -1;
+        if (execlp(program_path, program_path, nullptr) < 0) {
+          std::perror("exec failed");
+          return -1;
+        }
       }
     }
-
     return pid;
+  }
+
+  void handle_command(pid_t pid, std::string_view line) {
+    auto args = split(line, ' ');
+    auto command = args[0];
+
+    if (is_prefix(command, "continue")) {
+      resume(pid);
+      wait_on_signal(pid);
+    }
+    else {
+      std::cerr << "Unknown command\n";
+    }
+  }
+
+  std::vector<std::string> split(std::string_view str, char delimiter) {
+    std::vector<std::string> out{};
+    std::stringstream ss {std::string{str}};
+    std::string item;
+
+    while (std::getline(ss, item, delimiter)) {
+      out.push_back(item);
+    }
+
+    return out;
+  }
+
+  bool is_prefix(std::string_view str, std::string_view of) {
+    if (str.size() > of.size()) return false;
+    return std::equal(str.begin(), str.end(), of.begin());
+  }
+  void resume(pid_t pid) {
+    if (ptrace(PTRACE_CONT, pid, nullptr, nullptr) < 0) {
+      std::cerr << "Couldn't continue";
+      exit(-1);
+    }
+  }
+
+  void wait_on_signal(pid_t pid) {
+    int wait_status;
+    int options = 0;
+    if (waitpid(pid, &wait_status, options) < 0) {
+      std::perror("waitpid failed");
+      exit(-1);
+    }
   }
 }
 
 int main(int argc, const char** argv) {
   if (argc == 1) {
-    std::cerr << "No arguments given\n";
+    std::cerr << "No arguements given";
     return -1;
   }
 
@@ -58,7 +106,27 @@ int main(int argc, const char** argv) {
   int wait_status;
   int options = 0;
   if (waitpid(pid, &wait_status, options) < 0) {
-    std::perror("wait failed");
-    return -1;
+    std::perror("waitpid failed");
+  }
+
+  char* line = nullptr;
+  while ((line = readlin("ldb> ")) != nullptr) {
+    std::string_view line_str;
+
+    if (line == std::string_view("")) {
+      free(line);
+      if (history_length > 0) {
+        line_str = history_list()[history_length - 1]->line;
+      }
+    }
+    else {
+      line_str = line;
+      add_history(line);
+      free(line);
+    }
+
+    if (!line_str.empty()) {
+      handle_command(pid, line_str);
+    }
   }
 }
